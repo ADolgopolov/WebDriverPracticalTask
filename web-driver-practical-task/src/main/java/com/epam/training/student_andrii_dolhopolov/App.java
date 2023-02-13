5a463c9475fa966b27a2f88a47ba64958e2452fd
Andrii Dolhopolov <dolgopolov_a@ukr.net>
2023-02-13 23:00:35 +0200

    Created new project.

---
+++ web-driver-practical-task/src/main/java/com/epam/training/student_andrii_dolhopolov/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_andrii_dolhopolov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
